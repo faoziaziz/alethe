23243ef6ea95b750d66a4bc3212e35c7e1f80005
azizfaozi <admin@labseni.com>
2018-12-12 10:56:59 +0700

    masukkin wx

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 # ini program utamanya
 import matplotlib 
 print "ini program utama"
+
+import wx
+
+app = wx.App()
+
+frame = wx.Frame(None, title='Simple APplication')
+frame.Show()
+
+app.MainLoop()
